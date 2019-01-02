516bf933a3b68a86b79f5db59084cb5ca06145ba
Johnathan <reddjohnathan@gmail.com>
2019-01-02 16:53:07 -0600

    created the class

---
+++ app.js
@@ -0,0 +1,12 @@
+
+class tomagotchiPet{
+	constructor(petName, hunger, sleepiness, boredom, age){
+		this.petName = petName
+		this.hunger = hunger
+		this.sleepiness = sleepiness
+		this.boredom = boredom
+		this.age = age
+	}
+}
+
+
