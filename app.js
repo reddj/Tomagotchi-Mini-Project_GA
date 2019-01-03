6581f2d42c8223390cd17274ba14495fc404154c
Johnathan <reddjohnathan@gmail.com>
2019-01-02 20:24:58 -0600

    added some CSS. trying to get the button to work

--- app.js
+++ app.js
@@ -10,3 +10,15 @@ class tomagotchiPet{
 }
 
 
+let pet = new tomagotchiPet("New Pet", 0, 0, 0, 0)
+$(".petStats").append("<p>" + pet.petName + "</p>" + "<p>" + pet.hunger + "</p>" + "<p>" + pet.sleepiness + "</p>" + "<p>" + pet.boredom + "</p>" + "<p>" + pet.age + "</p>")
+
+$("#change-pet-name").click(function(){
+  let petName = $("#change-pet-name").val()
+  if(petName === ""){
+		alert("Name cannot be blank.")
+	}else{
+	$("#petName").append($petName)
+	$("#petName").val("")
+}
+})
