6a16732b0deb8861040e397172a932ad0b31cb38
Johnathan <reddjohnathan@gmail.com>
2019-01-04 15:15:34 -0600

    After showing the class. Changes with Tyler.

--- app.js
+++ app.js
@@ -11,14 +11,102 @@ class tomagotchiPet{
 
 
 let pet = new tomagotchiPet("New Pet", 0, 0, 0, 0)
-$(".petStats").append("<p>" + pet.petName + "</p>" + "<p>" + pet.hunger + "</p>" + "<p>" + pet.sleepiness + "</p>" + "<p>" + pet.boredom + "</p>" + "<p>" + pet.age + "</p>")
+$(".petStats").append("<p>" + pet.petName + "</p>" + "<p> Hunger: " + pet.hunger + "</p>" + "<p> Sleepiness: " + pet.sleepiness + "</p>" + "<p> Boredom: " + pet.boredom + "</p>" + "<p> Age: " + pet.age + "</p>")
 
 $("#change-pet-name").click(function(){
-  let petName = $("#change-pet-name").val()
+  let petName = $("#change-petName-input").val()
   if(petName === ""){
-		alert("Name cannot be blank.")
+		alert("Please add a name for your pet.")
 	}else{
-	$("#petName").append($petName)
-	$("#petName").val("")
+	pet.petName = petName
+	refresh()
 }
 })
+
+let refresh = function(){
+	$(".petStats").empty()
+	$(".petStats").append(
+		`<p>${pet.petName}</p>
+		 <p>Hunger: ${pet.hunger}</p>
+		 <p>Sleepiness: ${pet.sleepiness}</p>
+		 <p>Boredom: ${pet.boredom}</p>
+		 <p>Age: ${pet.age}</p>`)
+		//"<p>" + pet.petName + "</p>" + "<p> Hunger: " + pet.hunger + "</p>" + "<p> Sleepiness: " + pet.sleepiness + "</p>" + "<p> Boredom: " + pet.boredom + "</p>" + "<p> Age: " + pet.age + "</p>")
+}
+
+let increase = function(){
+	setTimeout(function(){
+		pet.hunger += 1
+		if(pet.hunger < 10){
+		$(".petStats").empty()
+		$(".petStats").append("<p>" + pet.petName + "</p>" + "<p> Hunger: " + pet.hunger + "</p>" + "<p> Sleepiness: " + pet.sleepiness + "</p>" + "<p> Boredom: " + pet.boredom + "</p>" + "<p> Age: " + pet.age + "</p>")
+        increase()
+	}}, 5000)
+}
+
+let increase2 = function(){
+	setTimeout(function(){
+		pet.sleepiness += 1
+		if(pet.sleepiness < 10){
+		$(".petStats").empty()
+		$(".petStats").append("<p>" + pet.petName + "</p>" + "<p> Hunger: " + pet.hunger + "</p>" + "<p> Sleepiness: " + pet.sleepiness + "</p>" + "<p> Boredom: " + pet.boredom + "</p>" + "<p> Age: " + pet.age + "</p>")
+        increase2();
+	}}, 5000)
+}
+
+let increase3 = function(){
+	setTimeout(function(){
+		
+		pet.boredom += 1
+		if(pet.boredom < 10){
+		$(".petStats").empty()
+		$(".petStats").append("<p>" + pet.petName + "</p>" + "<p> Hunger: " + pet.hunger + "</p>" + "<p> Sleepiness: " + pet.sleepiness + "</p>" + "<p> Boredom: " + pet.boredom + "</p>" + "<p> Age: " + pet.age + "</p>")
+        increase3();
+	}}, 5000)
+}
+
+let increase4 = function(){
+	setTimeout(function(){
+		pet.age += 1
+		death()
+		$(".petStats").empty()
+		$(".petStats").append("<p>" + pet.petName + "</p>" + "<p> Hunger: " + pet.hunger + "</p>" + "<p> Sleepiness: " + pet.sleepiness + "</p>" + "<p> Boredom: " + pet.boredom + "</p>" + "<p> Age: " + pet.age + "</p>")
+        increase4();
+	}, 5000)
+}
+
+$("#feed-pet").click(function(){
+ if(pet.hunger !== 10){
+   pet.hunger--
+   refresh()
+ }
+})
+
+$("#sleep").click(function(){
+ if(pet.sleepiness !== 10){
+   pet.sleepiness--
+   refresh()
+ }
+})
+
+$("#play-with-pet").click(function(){
+ if(pet.boredom !== 10){
+   pet.boredom--
+   refresh()
+ }
+})
+
+let death = function(){
+	if(pet.hunger >= 10 || pet.sleepiness >= 10 || pet.boredom >= 10){
+		$(".pet").fadeOut()
+	}
+}
+
+increase()
+increase2()
+increase3()
+increase4()
+
+
+
+
